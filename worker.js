a2d6ff79f13c18ff2e7178fbe7e1e82847c9d6bf
Aagaman1229 <aagaman4321@gmail.com>
2026-06-12 12:45:48 +0545

    Fix worker SPA routing - serve index.html for extension-less paths

--- worker.js
+++ worker.js
@@ -1,7 +1,15 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
+
+    if (url.pathname.match(/\.[a-zA-Z0-9]+$/)) {
+      try {
+        const response = await env.ASSETS.fetch(request);
+        if (response.status === 200) return response;
+      } catch (e) {}
+    }
+
     url.pathname = '/index.html';
-    return env.ASSETS.fetch(new Request(url, request));
+    return env.ASSETS.fetch(url.toString());
   },
 };

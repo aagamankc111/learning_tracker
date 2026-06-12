1aeb2e98e5fa58b9cf4fedd6d82e9b7a6e8da4da
Aagaman1229 <aagaman4321@gmail.com>
2026-06-12 12:29:26 +0545

    Simplify worker - serve index.html for all non-asset routes

--- worker.js
+++ worker.js
@@ -1,14 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-
-    try {
-      const response = await env.ASSETS.fetch(request);
-      if (response.status !== 404) return response;
-    } catch (e) {
-      // asset not found, fall through to SPA fallback
-    }
-
     url.pathname = '/index.html';
     return env.ASSETS.fetch(new Request(url, request));
   },
